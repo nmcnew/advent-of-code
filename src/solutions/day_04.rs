f48407efb37c358ac8f3983719ac3f7ff3462b4e
nmcnew <nmcnew@proton.me>
2023-12-04 01:50:15 -0700

    Cleanup some refs

--- src/solutions/day_04.rs
+++ src/solutions/day_04.rs
@@ -16,14 +16,12 @@ pub fn part2(input: &String) -> i32 {
         .collect::<Vec<_>>();
 
     for card_id in 0..cards.len() {
-        let card = &cards[card_id];
-        let card_count = card.count;
-        let mut limit = card_id + card.win_count as usize;
+        let mut limit = card_id + cards[card_id].win_count as usize;
         if limit >= cards.len() {
             limit = cards.len() - 1;
         }
         for n in card_id + 1..=limit {
-            cards[n].count = cards[n].count + card_count;
+            cards[n].count = cards[n].count + cards[card_id].count;
         }
     }
     let result = cards.iter().map(|c| c.count).sum();
@@ -32,18 +30,10 @@ pub fn part2(input: &String) -> i32 {
 
 fn vec_of_nums(input: &str) -> Vec<i32> {
     return input
-        .trim()
         .split(" ")
         .filter(|n| !n.is_empty())
         .map(|s| s.trim().parse::<i32>().unwrap())
         .collect::<Vec<_>>();
-}
-
-fn get_times_won(winning_numbers: Vec<i32>, own_numbers: Vec<i32>) -> i32 {
-    return own_numbers
-        .iter()
-        .map(|c| if winning_numbers.contains(c) { 1 } else { 0 })
-        .sum();
 }
 
 #[derive(Debug, PartialEq)]
@@ -73,11 +63,12 @@ impl FromStr for Card {
             .trim()
             .parse::<i32>()
             .map_err(|_| ParsePointError)?;
-        let winning_string = game_and_winners.next().ok_or(ParsePointError)?;
-        let own_numbers = thing.next().ok_or(ParsePointError)?;
-        let winning_numbers = vec_of_nums(winning_string);
-        let scratched_numbers = vec_of_nums(own_numbers);
-        let win_count = get_times_won(winning_numbers.clone(), scratched_numbers.clone());
+        let winning_numbers = vec_of_nums(game_and_winners.next().ok_or(ParsePointError)?);
+        let scratched_numbers = vec_of_nums(thing.next().ok_or(ParsePointError)?);
+        let win_count = scratched_numbers
+            .iter()
+            .map(|c| if winning_numbers.contains(c) { 1 } else { 0 })
+            .sum();
         return Ok(Card {
             game_id: game_id,
             win_count,
